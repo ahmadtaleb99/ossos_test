71d9e3f77fca83c703b0ddb65b9420757bf54917
unknown <ahmad.taleb1@hotmail.com>
2023-11-27 18:44:41 +0300

    create the domain structure

--- lib/data/network/api_client.dart
+++ lib/data/network/api_client.dart
@@ -5,7 +5,7 @@ import 'package:ossos_test/data/responses/pokemon_response.dart';
 abstract class ApiClient {
 
 
-  Future<PokemonBaseResponse>  getPokemonns(String page);
+    Future<PokemonBaseResponse>  getPokemonns(String page);
 
 
 }
---
+++ lib/domain/model/pokemons_repository.dart
@@ -0,0 +1,8 @@
+
+import 'package:dartz/dartz.dart';
+import 'package:ossos_test/data/network/failure.dart';
+import 'package:ossos_test/domain/model/models.dart';
+
+abstract class PokemonsRepository   {
+  Future<Either<Failure,List<Pokemon>>> getOffers({required String page,});
+}
