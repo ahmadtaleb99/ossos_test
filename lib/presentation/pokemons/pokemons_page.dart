6ab49a62a046878e174b4bc245cfa9a8932805f5
unknown <ahmad.taleb1@hotmail.com>
2023-11-28 09:47:36 +0300

    added bloc to pokemons page

--- lib/presentation/pokemons/pokemons_page.dart
+++ lib/presentation/pokemons/pokemons_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:ossos_test/domain/model/models.dart';
+import 'package:ossos_test/presentation/pokemons/bloc/pokemons_bloc.dart';
 import 'package:ossos_test/presentation/pokemons/pokemon_card.dart';
 import 'package:ossos_test/presentation/resources/strings_manager.dart';
 
@@ -8,14 +12,40 @@ class PokemonsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(AppStrings.pokemons),
+        appBar: AppBar(
+          title: Text(AppStrings.pokemons),
+        ),
+        body: BlocBuilder<PokemonsBloc, PokemonsState>(
+          builder: (context, state) {
+
+            if(state is PokemonsLoaded)
+            return PokemonsList(pokemoms:state.pokemons,
+              pagingController: context.read<PokemonsBloc>().pagingController,
+
+            );
+
+
+            return CircularProgressIndicator();
+          },
+        )
+    );
+  }
+}
+
+
+class PokemonsList extends StatelessWidget {
+  const PokemonsList({Key? key, required this.pokemoms,required this.pagingController})
+      : super(key: key);
+  final List<Pokemon> pokemoms;
+  final PagingController<int, Pokemon> pagingController;
+
+  @override
+  Widget build(BuildContext context) {
+    return PagedListView<int, Pokemon>(
+      pagingController: pagingController,
+      builderDelegate: PagedChildBuilderDelegate<Pokemon>(
+        itemBuilder: (context, item, index) => PokemonCard(),
       ),
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context,index) {
-        return PokemonCard();
-      }),
     );
   }
 }
