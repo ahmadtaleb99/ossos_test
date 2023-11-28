0ded9dbe9b33a5ea168ac21b5980747775c63cee
unknown <ahmad.taleb1@hotmail.com>
2023-11-28 13:36:07 +0300

    removed unnecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:ossos_test/app/dependency_injection.dart';
 Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
-  HttpOverrides.global =  MyHttpOverrides();
 
   await initAppModules();
 
@@ -19,11 +18,3 @@ Future<void> main() async {
 }
 
 
-
-class MyHttpOverrides extends HttpOverrides{
-  @override
-  HttpClient createHttpClient(SecurityContext ? context){
-    return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
-  }
-}
